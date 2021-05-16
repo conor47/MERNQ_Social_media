fcfab5dee4205922c1624a3e3efb3c4e2fedf780
conor47 <63269594+conor47@users.noreply.github.com>
2021-05-16 15:23:31 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB:
-    "mongodb+srv://fullstack:AlfieKinsealy36@@@cluster0.7vlnq.mongodb.net/mernq?retryWrites=true&w=majority",
-  SECRET_KEY: "kjsbdfjkbskebfksjdbfsdfmpqimnx",
-};
